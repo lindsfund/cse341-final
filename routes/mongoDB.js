c1004edf72ad023aa6f1b8e1eb586bd9cd7a1089
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-06 19:45:29 -0400

    rest client request file

--- routes/mongoDB.js
+++ routes/mongoDB.js
@@ -5,4 +5,24 @@ router.get('/', (req, res) => {
     res.send('MongoDB');
 });
 
+//get all resources
+//get one resource
+router.get('/:id', (req, res) => {
+    res.send('MongoDB');
+});
+
+//create new resource
+router.post('/', (req, res) => {
+    
+});
+
+//update resource
+router.put('/:id', (req, res) => {
+    
+});
+
+//delete resource
+router.delete('/:id', (req, res) => {
+    
+});
 module.exports = router;
--- routes/node.js
+++ routes/node.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
     res.send('NodeJS');
 });
 
+//get all resources
+//get one resource
+//create new resource
+//update resource
+//delete resource
 module.exports = router;
