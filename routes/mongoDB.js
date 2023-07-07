07be674b385fdeb71aa5bd7db9f71b8bd193ba35
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-07 09:09:25 -0400

    connect branch to VSCode

--- routes/mongoDB.js
+++ routes/mongoDB.js
@@ -4,6 +4,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.send('MongoDB');
 });
-
+//get all resources
+//get one resource
+//create new resource
+//update resource
+//delete resource
 
 module.exports = router;
