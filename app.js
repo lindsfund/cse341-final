a4526af413eec2b02407632482a10e8eaf4d5440
TeddyTjoe1990 <her21059@byui.edu>
2023-07-07 19:27:26 +0700

    made render controller and the route of it

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./db/connect');
+const db = require("./db/connect");
 
-
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const homeRouter = require('./routes/index');
-app.use('/', homeRouter);
+const homeRouter = require("./routes/index");
+app.use("/", homeRouter);
 
-const userRouter = require('./routes/users');
-app.use('/users', userRouter);
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
 
-const mongoRouter = require('./routes/mongoDB');
-app.use('/mongodb', mongoRouter);
+const mongoRouter = require("./routes/mongoDB");
+app.use("/mongodb", mongoRouter);
 
-const nodeRouter = require('./routes/node');
-app.use('/node', nodeRouter);
+const nodeRouter = require("./routes/node");
+app.use("/node", nodeRouter);
 
-const renderRouter = require('./routes/render');
-app.use('/render', renderRouter);
-
+const renderRouter = require("./routes/render");
+app.use("/render", renderRouter);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
--- routes/render.js
+++ routes/render.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const renderController = require('../controllers/render');
+//const validation = require('../midware/validate');
+//const {requiresAuth} = require('express-openid-connect');
+//const {isAuthenticated} = require('../midware/authenticate');
 
-router.get('/', (req, res) => {
-    res.send('RENDER');
-});
+router.get('/', renderController.getAllRenderData);
+
+router.get('/:id', renderController.getRenderById);
+
+router.post('/', requiresAuth(), isAuthenticated, validation.saveRender, renderController.createRenderData);
+
+router.put('/:id', requiresAuth(), isAuthenticated, validation.saveRender, renderController.updateRenderData);
+
+router.delete('/:id', requiresAuth(), isAuthenticated, renderController.deleteRenderData);
 
 module.exports = router;
