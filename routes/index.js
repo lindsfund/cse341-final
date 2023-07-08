a599238919c76b194ac7fe6ef642aa65e8ea3682
TeddyTjoe1990 <her21059@byui.edu>
2023-07-08 22:04:50 +0700

    preparing routes

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/render', require('./render'));
+//router.use('/mongoDB', require('./mongoDB'));
+//router.use('/node', require('./node'));
+//router.use('/user', require('./user'));
 
 module.exports = router;
