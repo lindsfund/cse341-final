e5aaf882aa669a32e288e6402a42e33df4603ab7
TeddyTjoe1990 <her21059@byui.edu>
2023-07-08 19:00:29 +0700

    trying something to deployment

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,5 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/render', require('./render'));
-router.use('/mongodb', require('./mongodb'));
-router.use('/node', require('./node'));
-router.use('/users', require('./users'));
 
 module.exports = router;
