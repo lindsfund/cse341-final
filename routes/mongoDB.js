fbc9b593c184b8022007936c2d537019a6702b0e
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-10 18:54:49 -0400

    connected to DB

--- routes/mongoDB.js
+++ routes/mongoDB.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const mongoDbController = require('../controllers/mongoDb');
 
-router.get('/', (req, res) => {
-    res.send('MongoDB');
-});
+router.get('/', mongoDbController.getAllMongoDbData);
 
+router.get('/:id', mongoDbController.getMongoDbById);
+
+router.post('/', mongoDbController.createMongoDbData);
+
+router.put('/:id', mongoDbController.updateMongoDbData);
+
+router.delete('/:id', mongoDbController.deleteMongoDbData);
 
 module.exports = router;
