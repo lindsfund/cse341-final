4d72b8b49a0e4f2ba9eb5c232f298f8d22d078e1
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-14 15:23:35 -0400

    update routes

--- routes/mongoDB.js
+++ routes/mongoDB.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 const mongoDbController = require('../controllers/mongoDb');
 
+//get all resources
 router.get('/', mongoDbController.getAllMongoDbData);
 
+//get one resource
 router.get('/:id', mongoDbController.getMongoDbById);
 
+//create new resource
 router.post('/', mongoDbController.createMongoDbData);
 
+//update resource
 router.put('/:id', mongoDbController.updateMongoDbData);
 
+//delete resource
 router.delete('/:id', mongoDbController.deleteMongoDbData);
 
 module.exports = router;
