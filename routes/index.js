240001d832c15e9095227afd602f619ad1c8a548
TeddyTjoe1990 <her21059@byui.edu>
2023-07-14 19:10:34 +0700

    update the index route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/render', require('./render'));
-//router.use('/mongoDB', require('./mongoDB'));
-//router.use('/node', require('./node'));
-//router.use('/user', require('./user'));
+router.use('/mongoDB', require('./mongoDB'));
+router.use('/node', require('./node'));
+router.use('/user', require('./user'));
+
+router.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 module.exports = router;
