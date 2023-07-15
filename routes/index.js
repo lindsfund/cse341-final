c4c711fdf8609f2f94e04691b512d4a0402ff59f
TeddyTjoe1990 <her21059@byui.edu>
2023-07-15 19:09:29 +0700

    edit some bugs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/render', require('./render'));
-router.use('/mongoDB', require('./mongoDB'));
+router.use('/mongodb', require('./mongoDB'));
 router.use('/node', require('./node'));
 router.use('/user', require('./user'));
 
