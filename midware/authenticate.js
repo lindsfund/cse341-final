d6ba4e31bc48f22e28b6522b2385b4ce1195382b
TeddyTjoe1990 <her21059@byui.edu>
2023-07-16 01:29:44 +0700

    edited issuerBaseURL

--- midware/authenticate.js
+++ midware/authenticate.js
@@ -7,7 +7,7 @@ const config = {
   secret: 'a long, randomly-generated string stored in env',
   baseURL: 'https://course-learning-resources.onrender.com',
   clientID: 'OoIT2xKvqNBn0haPIJqQbeb7VahEeWTz',
-  issuerBaseURL: 'dev-nkraj6kh6thnzrsh.us.auth0.com'
+  issuerBaseURL: 'https://dev-nkraj6kh6thnzrsh.us.auth0.com'
 };
 
 const isAuthenticated = (req, res, next) => {
