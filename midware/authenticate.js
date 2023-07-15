ed9b5e2c3d4d9828988bfb7c547b7723862aa867
TeddyTjoe1990 <her21059@byui.edu>
2023-07-15 19:50:14 +0700

    update configuration

--- midware/authenticate.js
+++ midware/authenticate.js
@@ -4,10 +4,10 @@ dotenv.config();
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_RENDER_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://course-learning-resources.onrender.com',
+  clientID: 'OoIT2xKvqNBn0haPIJqQbeb7VahEeWTz',
+  issuerBaseURL: 'dev-nkraj6kh6thnzrsh.us.auth0.com'
 };
 
 const isAuthenticated = (req, res, next) => {
