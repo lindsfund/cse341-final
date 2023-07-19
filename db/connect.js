d7c18b2185b68f2c462956080abd2fe747d07904
TeddyTjoe1990 <her21059@byui.edu>
2023-07-19 07:33:38 +0700

    adding some tests

--- db/connect.js
+++ db/connect.js
@@ -5,19 +5,30 @@ const MongoClient = require('mongodb').MongoClient;
 
 let _db;
 
-const initDb = (callback) => {
+//const initDb = (callback) => {
+//  if (_db) {
+//    console.log('Database is already initialized!');
+//    return callback(null, _db);
+//  }
+//  MongoClient.connect(process.env.MONGODB_URI)
+//    .then((client) => {
+//      _db = client;
+//      callback(null, _db);
+//    })
+//    .catch((err) => {
+//      callback(err);
+//    });
+//};
+
+const initDb = async () => {
   if (_db) {
     console.log('Database is already initialized!');
-    return callback(null, _db);
+    return Promise.resolve(_db);
   }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
+
+  const client = await MongoClient.connect(process.env.MONGODB_URI);
+  _db = client;
+  return _db;
 };
 
 const getDb = () => {
