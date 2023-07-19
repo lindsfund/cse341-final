6d3f20d486f9365269a4ce1dc8589fbb894c162c
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-19 05:34:20 -0400

    hjfjh

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const app = express();
-const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+const cookieParser = require("cookie-parser")
+const passport = require('passport')
+const { configureGoogleStrategy, configureLocalStrategy } = require('./config/passport');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const { checkJwtToken } = require("./middleware/authenticate");
 
 
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(checkJwtToken);
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
+
+// app.use(passport.initialize());
+// configureGoogleStrategy(passport);
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+// passport.deserializeUser((user, done) => {
+//   done(null,user);
+// });
 
 const homeRouter = require('./routes/index');
 app.use('/', homeRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/auth');
+
 
 const userRouter = require('./users');
 const dbRouter =require('./mongoDB');
@@ -7,8 +9,24 @@ const nodeRouter = require('./node');
 const renderRouter = require('./render');
 
 router.get('/', (req, res) => {
-    res.send('HOMEPAGE')
+    res.send('HOMEPAGE');
+    if (res.locals.loggedin) {
+        res.send(`Logged in as ${res.locals.user.firstName}.`);
+    } else {
+        res.send('Logged out.');
+    }
 });
+
+//loggin and out routes
+router.get('/login', authController.authenticateWithGoogle);
+router.get('/google/callback', authController.handleGoogleCallback);
+router.post('/login', authController.handleLocalAuthentication);
+router.get('/logout', (req, res, next) => {
+    res.clearCookie('jwt');
+    delete res.locals.user;
+    delete res.locals.loggedin;
+    res.redirect('/');
+})
 
 router.use('/users', userRouter);
 router.use('/mongoDB', dbRouter);
